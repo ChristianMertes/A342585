46777566efb52a89a64be05655f6dfc352f82cb4
Christian Leichsenring <christian.leichsenring@semalytix.com>
2022-11-10 13:48:12 +0100

    first implementation

---
+++ plot_sequence.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""
+Script to generate a scatter plot of the infinite series https://oeis.org/A342585
+with the number of terms to generate as the first parameter and the output file as a second.
+"""
+import sys
+from collections import Counter
+from itertools import count
+import sys
+import matplotlib.pyplot as plt
+
+def aupton(target: int):
+    """adapted from Michael S. Branicky's code from Jun 12 2021"""
+    num: int = 0
+    alst: list[int] = [0]
+    inventory: Counter[int] = Counter([0])
+    for n in range(2, target + 1):
+        c = inventory[num]
+        num = 0 if c == 0 else num + 1
+        alst.append(c)
+        inventory.update([c])
+    return alst
+
+
+if __name__ == '__main__':
+    target: int = int(sys.argv[1]) if len(sys.argv) > 1 else 10**7
+    plt.style.use('seaborn-v0_8')
+    #plt.plot(aupton(target), 'bo')
+    plt.scatter(range(1, target + 1), aupton(target), s=.1)
+    plt.savefig(sys.argv[2] if len(sys.argv) > 2 else f'plot_up_to_{target}.png')
---
+++ print_sequence.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""script to generate the infinite series https://oeis.org/A342585 with an optional parameter as the stopping point"""
+import sys
+from collections import Counter
+from itertools import count
+import sys
+
+def aupton(target: int):
+    """adapted from Michael S. Branicky's code from Jun 12 2021"""
+    num: int = 0
+    alst: list[int] = [0]
+    inventory: Counter[int] = Counter([0])
+    for n in range(2, target + 1) if target > 1 else count(2):
+        c = inventory[num]
+        num = 0 if c == 0 else num + 1
+        alst.append(c)
+        inventory.update([c])
+        yield c
+#    return alst
+
+
+if __name__ == '__main__':
+    target: int = int(sys.argv[1]) if len(sys.argv) > 1 else -1
+    for i in aupton(target):
+        print(i, end=', ')
+    print('\x08\x08')
